6c2fd757cddbdd48c02544ef2f9e2b538a1dc589
rios <rios>
2019-07-16 13:49:10 -0300

    Implements basic functionality of splash activity;

--- app/src/main/java/com/fpr0001/nasaimages/splash/SplashActivity.kt
+++ app/src/main/java/com/fpr0001/nasaimages/splash/SplashActivity.kt
@@ -5,13 +5,15 @@ import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
 import com.fpr0001.nasaimages.R
 import com.fpr0001.nasaimages.databinding.ActivitySplashBinding
+import com.fpr0001.nasaimages.search.SearchActivity
 import dagger.android.AndroidInjection
+import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
-        AndroidInjection.inject(this)
         super.onCreate(savedInstanceState)
-        val binding = DataBindingUtil.setContentView<ActivitySplashBinding>(this, R.layout.activity_splash)
+        setContentView(R.layout.activity_splash)
+        imageView.postDelayed({ SearchActivity.startActivity(this) }, 3000)
     }
 }
