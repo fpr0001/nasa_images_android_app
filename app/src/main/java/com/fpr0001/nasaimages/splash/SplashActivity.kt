7e49dc7a15b1973efc0ed225692e03c6373c12dd
rios <rios>
2019-07-16 16:24:14 -0300

    Improves splash screen efficiency;

--- app/src/main/java/com/fpr0001/nasaimages/splash/SplashActivity.kt
+++ app/src/main/java/com/fpr0001/nasaimages/splash/SplashActivity.kt
@@ -1,19 +1,14 @@
 package com.fpr0001.nasaimages.splash
 
 import android.os.Bundle
-import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.FragmentActivity
-import com.fpr0001.nasaimages.R
-import com.fpr0001.nasaimages.databinding.ActivitySplashBinding
 import com.fpr0001.nasaimages.search.SearchActivity
-import dagger.android.AndroidInjection
-import kotlinx.android.synthetic.main.activity_splash.*
 
 class SplashActivity : FragmentActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_splash)
-        imageView.postDelayed({ SearchActivity.startActivity(this) }, 3000)
+        SearchActivity.startActivity(this)
+        overridePendingTransition(android.R.anim.fade_in, android.R.anim.fade_out)
     }
 }
--- app/src/main/java/com/fpr0001/nasaimages/utils/BaseActivity.kt
+++ app/src/main/java/com/fpr0001/nasaimages/utils/BaseActivity.kt
@@ -1,5 +1,7 @@
 package com.fpr0001.nasaimages.utils
 
+import android.graphics.Color
+import android.os.Bundle
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
@@ -11,6 +13,11 @@ import com.fpr0001.nasaimages.R
 abstract class BaseAppCompatActivity : AppCompatActivity(), MvpView {
 
     protected var progressBar: ProgressBar? = null
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        window?.navigationBarColor = Color.BLACK
+    }
 
     override fun setContentView(layoutResID: Int) {
         super.setContentView(layoutResID)
